1ed8b86d0d2fb896b6bd059ed31511f85b28e88e
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-04-15 03:21:25 -0300

    Procedimientos y funciones para estructura-arbol.

--- EDB.cpp
+++ EDB.cpp
@@ -7,19 +7,75 @@ typedef struct tnodo *arbol;
 
 struct tnodo
 {
-    int dato;
-    arbol izquierda;
-    arbol derecha;
+    int dat;
+    arbol left;
+    arbol right;
 };
 
-void initT(arbol &arb)
+void initT(arbol &tree)
 {
-    arb = NULL;
+    tree = NULL;
+}
+
+void create_nodo(arbol &neww)
+{
+    neww = new tnodo;
+    if (neww != NULL)
+    {
+        cout << "Ingrese numero: ";
+        cin >> neww->dat;
+        neww->left = NULL;
+        neww->right = NULL;
+    }
+    else
+    {
+        cout << "Memoria llena" << endl;
+    }
+}
+
+void insertA(arbol &a, arbol nuevo)
+{
+    if (a == NULL)
+    {
+        a = nuevo;
+    }
+    else
+    {
+        if (nuevo->dat < a->dat)
+        {
+            insertA(a->left, nuevo);
+        }
+        else
+        {
+            insertA(a->right, nuevo);
+        }
+    }
+}
+
+void preOrden(arbol a)
+{
+    if (a != NULL)
+    {
+        cout << a->dat << " ";
+        preOrden(a->left);
+        preOrden(a->right);
+    }
 }
 
 int main ()
 {
-    arbol tree;
-    initT(a);
+    arbol tree, anew;
+    initT(tree);
+    int ingreso = 1;
+    while(ingreso <= 6)
+    {
+        create_nodo(anew);
+        insertA(tree, anew);
+        ingreso++;
+    }
+
+    cout << "DATOS EN PRE - ORDEN" << endl;
+    preOrden(tree);
+
     return 0;
 }
